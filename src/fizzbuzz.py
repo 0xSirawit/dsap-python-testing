ad02a6e8ccf65babf81cc441bad221ead3a618d5
Sirawit Loywirat <sloywirat@gmail.com>
2025-03-05 22:06:49 +0700

    Add type hints to fizzbuzz and is_prime_list functions

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,4 +1,4 @@
-def fizzbuzz(number):
+def fizzbuzz(number: int) -> str:
     if number % 3 == 0 and number % 5 == 0:
         return "FizzBuzz"
     if number % 3 == 0:
--- src/number_utils.py
+++ src/number_utils.py
@@ -1,4 +1,4 @@
-def is_prime_list(numbers):
+def is_prime_list(numbers: int) -> bool:
     for num in numbers:
         for n in range(2, num):
             if num % n == 0:
