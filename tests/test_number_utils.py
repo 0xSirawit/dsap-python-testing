fcf352aefe7cbad2c5f22d74ce06b1953e055f48
Sirawit Loywirat <sloywirat@gmail.com>
2025-03-05 21:21:14 +0700

    Add initial implementation of prime number utility

---
+++ tests/test_number_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from src.number_utils import is_prime_list
+
+
+class PrimeListTest(unittest.TestCase):
+    def test_give_1_2_3_is_not_prime(self):
+        prime_list = [1, 2, 3]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_2_3_5_is_prime(self):
+        prime_list = [2, 3, 5]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
