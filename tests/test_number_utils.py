a7eec5a45d48443243b8eae2d9dd4ea10161e032
Sirawit Loywirat <sloywirat@gmail.com>
2025-03-05 21:28:55 +0700

    Add unit tests for prime number utility

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -12,3 +12,28 @@ class PrimeListTest(unittest.TestCase):
         prime_list = [2, 3, 5]
         is_prime = is_prime_list(prime_list)
         self.assertTrue(is_prime)
+
+    def test_give_13_17_19_is_prime(self):
+        prime_list = [13, 17, 19]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_give_1_2_4_5_7_is_mixed(self):
+        prime_list = [1, 2, 4, 5, 7]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_9_is_not_prime(self):
+        prime_list = [9]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_2_is_prime(self):
+        prime_list = [2]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_give_empty(self):
+        prime_list = []
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
